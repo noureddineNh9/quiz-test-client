5f234901b9a3c5d76b66d46d40e0b5719954a5f7
noureddine haddad <noureddinehaddad17@gmail.com>
2023-02-25 12:26:32 +0100

    update prod env

--- src/configs/EnvironmentConfig.js
+++ src/configs/EnvironmentConfig.js
@@ -1,26 +1,26 @@
 const dev = {
-   API_ENDPOINT_URL: "http://localhost:5000",
+  API_ENDPOINT_URL: "http://localhost:5000",
 };
 
 const prod = {
-   API_ENDPOINT_URL: "http://141.145.209.172:5000",
+  API_ENDPOINT_URL: "https://quiz-test-api.nh9.dev",
 };
 
 const test = {
-   API_ENDPOINT_URL: "http://localhost:5000",
+  API_ENDPOINT_URL: "http://localhost:5000",
 };
 
 const getEnv = () => {
-   switch (process.env.NODE_ENV) {
-      case "development":
-         return dev;
-      case "production":
-         return prod;
-      case "test":
-         return test;
-      default:
-         break;
-   }
+  switch (process.env.NODE_ENV) {
+    case "development":
+      return dev;
+    case "production":
+      return prod;
+    case "test":
+      return test;
+    default:
+      break;
+  }
 };
 
 export const env = getEnv();

571ccaa437dfa572fcef4e559c3ab38ef3e70bea
noureddine haddad <noureddinehaddad17@gmail.com>
2023-02-25 16:55:18 +0100

    fixes styles issue

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./assets/css/tailwind.css";
+import "./index.css";
 import "./styles/main.scss";
 
 ReactDOM.render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>,
-   document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
